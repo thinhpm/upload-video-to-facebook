fdf4996a25d19251aabfe9f3e0c717f64f59bda1
thinhlucy <thinhpm@lucy.ne.jp>
2019-05-28 13:54:08 +0700

    add

--- main.js
+++ main.js
@@ -8,35 +8,39 @@ function getData() {
     var title = datas['title']
     var des = datas['des']
     var access_token = datas['token'];
-    var video = datas['video']; 
-    
+    var video = datas['video'];
+    var thumb = datas['thumb'];
+    var tags = datas['tags'];
+
     return {
         'id': id,
         'title': title,
         'des': des,
         'access_token': access_token,
-        'video': video
+        'video': video,
+        'thumb': thumb,
+        'tags': tags
     }
 }
 
 function handle() {
     var datas = getData();
     args = {
-        token: datas['access_token'], // with the permission to upload
-        id: datas['id'], //The id represent {page_id || user_id || event_id || group_id}
-        stream: fs.createReadStream(datas['video']), //path to the video,
-        title: datas['title'],
-        description: datas['des'],
-        // thumb: {
-        //     value: fs.createReadStream('image.jpg'),
-        //     options: {
-        //         filename: 'image.jpg',
-        //         contentType: 'image/jpg'
-        //     }
-        // }
-        // if you want the default thumb from the video just remove the field
-        // you can add any extra fields from the api https://developers.facebook.com/docs/graph-api/reference/page/videos/#Creating
-        // all keys except token, id, stream are passed to the final request
+         token: datas['access_token'], // with the permission to upload
+         id: datas['id'], //The id represent {page_id || user_id || event_id || group_id}
+         stream: fs.createReadStream(datas['video']), //path to the video,
+         title: datas['title'],
+         description: datas['des'],
+         thumb: {
+             value: fs.createReadStream(datas['thumb']),
+             options: {
+                 filename: datas['thumb'],
+                 contentType: 'image/jpg'
+             }
+         }
+//         if you want the default thumb from the video just remove the field
+//         you can add any extra fields from the api https://developers.facebook.com/docs/graph-api/reference/page/videos/#Creating
+//         all keys except token, id, stream are passed to the final request
     };
          
     fbUpload(args).then((res) => {
